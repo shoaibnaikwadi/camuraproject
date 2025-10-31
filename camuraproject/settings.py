f2b01ae7c713ad179aa439e12693d8167d78a57b
shoiab <naikwadishoaib@gmail.com>
2025-10-31 06:55:39 -0700

    a

--- camuraproject/settings.py
+++ camuraproject/settings.py
@@ -146,3 +146,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
--- home/views.py
+++ home/views.py
@@ -30,3 +30,5 @@ def register(request):
 # @login_required
 # def dashboard(request):
 #     return render(request, 'dashboard.html')
+
+
