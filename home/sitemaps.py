98fa0445b373cd126d7699b0d9c31da533eb6575
shoiab <naikwadishoaib@gmail.com>
2025-11-20 10:44:07 +0530

    a

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Product  # adjust model name if different
+from .models import ComboProduct  # adjust model name if different
 
 
 class StaticViewSitemap(Sitemap):
