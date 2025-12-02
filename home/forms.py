60b3fca49c7f78e47c9aca4b8b805104630d5053
shoiab <naikwadishoaib@gmail.com>
2025-12-02 19:33:22 +0530

    form

--- home/forms.py
+++ home/forms.py
@@ -47,16 +47,68 @@ class CustomerProfileForm(forms.ModelForm):
         }
 
 
+# from django import forms
+# from .models import ServiceBooking
+
+# class ServiceBookingForm(forms.ModelForm):
+#     class Meta:
+#         model = ServiceBooking
+#         fields = "__all__"
+#         widgets = {
+#             "preferred_date": forms.DateInput(attrs={"type": "date"}),
+#             "preferred_time": forms.TimeInput(attrs={"type": "time"}),
+#             "problem_description": forms.Textarea(attrs={"rows": 3}),
+#             "address": forms.Textarea(attrs={"rows": 2}),
+#         }
+
+
+
 from django import forms
 from .models import ServiceBooking
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
+
 
 class ServiceBookingForm(forms.ModelForm):
     class Meta:
         model = ServiceBooking
         fields = "__all__"
+
         widgets = {
-            "preferred_date": forms.DateInput(attrs={"type": "date"}),
-            "preferred_time": forms.TimeInput(attrs={"type": "time"}),
-            "problem_description": forms.Textarea(attrs={"rows": 3}),
-            "address": forms.Textarea(attrs={"rows": 2}),
+            "preferred_date": forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}
+            ),
+            "preferred_time": forms.TimeInput(
+                attrs={"type": "time", "class": "form-control"}
+            ),
+            "problem_description": forms.Textarea(
+                attrs={"rows": 3, "class": "form-control"}
+            ),
+            "address": forms.Textarea(
+                attrs={"rows": 2, "class": "form-control"}
+            ),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+
+        self.helper.layout = Layout(
+            "name",
+            "mobile",
+            "email",
+            "problem_description",
+            "service_type",
+
+            Row(
+                Column("preferred_date", css_class="col-md-6"),
+                Column("preferred_time", css_class="col-md-6"),
+            ),
+
+            "address",
+            "attachment",
+
+            Submit("submit", "Submit Booking", css_class="btn btn-primary w-100 mt-3")
+        )
