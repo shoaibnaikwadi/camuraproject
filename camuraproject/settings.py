49416f015818d48e98259c6f71ec51fc8010c601
shoiab <naikwadishoaib@gmail.com>
2025-12-10 17:26:08 +0530

    a

--- camuraproject/settings.py
+++ camuraproject/settings.py
@@ -381,11 +381,11 @@ SMS_MESSAGE = config("SMS_MESSAGE", default="")
 # =========================
 # Email settings
 # =========================
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = config('EMAIL_PORT', cast=int)
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-# ADMIN_NOTIFICATION_EMAIL = config('ADMIN_NOTIFICATION_EMAIL')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+ADMIN_NOTIFICATION_EMAIL = config('ADMIN_NOTIFICATION_EMAIL')
--- camuraproject/wsgi.py
+++ camuraproject/wsgi.py
@@ -7,32 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camuraproject.settings')
-
-# application = get_wsgi_application()
-
-
 import os
-from decouple import Config, RepositoryEnv
+
 from django.core.wsgi import get_wsgi_application
 
-# Absolute path to project root (where manage.py and .env are located)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Path to .env file
-ENV_FILE = os.path.join(BASE_DIR, '.env')
-
-# Load .env manually (fix for Apache not reading .env)
-if os.path.exists(ENV_FILE):
-    config = Config(RepositoryEnv(ENV_FILE))
-
-# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camuraproject.settings')
 
-# Initialize WSGI application
 application = get_wsgi_application()
+
+
+
+
 
