c762c8841b92da4c7dd252aa08178e199b7f1f15
shoiab <naikwadishoaib@gmail.com>
2025-12-10 16:28:46 +0530

    a

--- camuraproject/wsgi.py
+++ camuraproject/wsgi.py
@@ -18,6 +18,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from decouple import Config, RepositoryEnv
+from django.core.wsgi import get_wsgi_application
 
 # Absolute path to project root (where manage.py and .env are located)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,5 +33,6 @@ if os.path.exists(ENV_FILE):
 # Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camuraproject.settings')
 
-from django.core.wsgi import get_wsgi_application
+# Initialize WSGI application
 application = get_wsgi_application()
+
